509338ba66fd5ffb0adcec9606d3fca75c9b596a
UtkarshiniBS <utkarshini.arora@blackstraw.ai>
2022-07-26 19:08:04 +0530

    init

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// for fetching all orders goto /orders route.
+
+// for fetching order between date range add query ?from=mm-dd-yyyy&end=mm-dd-yyyy
+
+// (ex: http://localhost:5000/orders?from=01-12-2022&to=07-25-2022) in front of /orders route
+
 const https = require("https");
 const http = require("http");
 
